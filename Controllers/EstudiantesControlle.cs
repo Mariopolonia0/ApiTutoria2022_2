55c5444277ccab09d936afea6eb5da1fb70d1507
Jose Enmanuel Jaquez Medina <jose_jaquez1@ucne.edu.do>
2022-07-26 21:40:23 -0400

    Api

---
+++ Controllers/EstudiantesControlle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTutoria2022_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class EstudiantesControlle : ControllersBase
+    {
+        private readonly Contexto _contexto;
+        public EstudiantesControlle(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        [HttpPost]
+        public List<EstudiantesDto> GetEstudiantes(int estudianteId)
+        {
+            return 
+                ).ToList();
+        }
+
+    }
+}
